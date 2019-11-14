21eb6e7cd6cd686e0906f64b833ba8b2ccc2066e
igo312 <794330684@qq.com>
2019-11-14 21:17:27 +0800

    Update WST_spider.py

--- scrapySP/spiders/WST_spider.py
+++ scrapySP/spiders/WST_spider.py
@@ -81,12 +81,12 @@ class QuotesSpider(scrapy.Spider):
 
     def url_genor(self):
         date_search = '12%20Months'
+	search_temp =  input('')
+	while search_temp != '':
+		search_list.append(search_temp)
 		search_temp =  input('')
-		while search_temp != '':
-			search_list.append(search_temp)
-			search_temp =  input('')
-		s = '%20'
-		search_result = s.join(search_list)
+	s = '%20'
+	search_result = s.join(search_list)
         url = 'https://www.washingtonpost.com/newssearch/?datefilter={}&query={}&sort=Relevance&startat={}#top'\
                  .format(date_search, search_result ,self.index)
         self.index += 20
